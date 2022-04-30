fb28eb74969616c9f62f1a3df00c6a206e55f6bd
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2022-04-30 13:15:52 -0300

    clase 4 II

--- Screens/Layout.js
+++ Screens/Layout.js
@@ -1,13 +1,16 @@
-import { TouchableOpacity, Button, TextInput, StyleSheet, Text, View } from 'react-native'
+import { Modal, FlatList, TouchableOpacity, Button, TextInput, StyleSheet, Text, View } from 'react-native'
 import { useState } from 'react'
-import Item from '../Components/Item'
+import TodoItem from '../Components/TodoItem'
 import { colors } from '../Styles/Colors'
 import ButtonContainer from '../Components/ButtonContainer'
 
 const Layout = () => {
 
-    const [input, setInput] = useState("")
-    const [todoList, setTodoList] = useState([])
+    const [input, setInput] = useState("");
+    const [todoList, setTodoList] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [idSelected, setIdSelected] = useState("");
+
     // console.log(input);
 
     const handleAdd = () => {
@@ -18,6 +21,17 @@ const Layout = () => {
     }
 
     console.log(todoList);
+
+    const handleDelete = () => {
+        const todosFiltrados = todoList.filter(item => item.id !== idSelected)
+        setTodoList(todosFiltrados);
+        setModalVisible(false);
+    }
+    const handleModal = (id) => {
+        setModalVisible(true)
+        setIdSelected(id);
+    }
+    const renderTodo = ({ item }) => <TodoItem onPress={handleModal} todo={item}></TodoItem>
 
 
     return (
@@ -39,13 +53,40 @@ const Layout = () => {
                 <Item item={{ id: 4, text: "Estudiar React Native" }}>  </Item>
                 <Item item={{ id: 5, text: "Estudiar React Native" }}>  </Item> */}
                 {todoList.length !== 0 ?
-                    todoList.map(item => {
-                        return <Item item={item} key={item.id} />
-                    })
+                    <FlatList
+                        data={todoList}
+                        keyExtractor={todo => todo.id}
+                        renderItem={renderTodo}
+                    />
+
+
+                    // No vamos a usar Map, se usa flatList, para que solo renderice lo que se ve
+                    //    todoList.map(item => <Item item={item} key={item.id} />)
                     :
                     <Text>No hay todos cargados</Text>
                 }
             </View>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <TouchableOpacity onPress={() => setModalVisible(false)}>
+                        <Text>
+                            X
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={handleDelete}>
+                        <Text>
+                            Eliminar todo
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
 
         </View>
 
@@ -61,12 +102,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
+        height: "100%",
     },
     topContainer: {
         flexDirection: "row",
         justifyContent: 'space-around',
         alignItems: 'center',
         padding: 10,
+        flex: 0.1,
     },
     input: {
         borderRadius: 8,
@@ -80,6 +123,20 @@ const styles = StyleSheet.create({
         width: "100%",
         padding: 10,
         borderRadius: 5,
+        //height: "90%", //en vez de flex se puede poner el %
+        flex: 0.9,
+    },
+    modalContainer: {
+        paddingLeft: 8,
+        marginTop: 50,
+        marginLeft: 50,
+        height: 200,
+        width: 300,
+        backgroundColor: colors.rosa,
+        borderWidth: 1,
+        borderRadius: 6,
+        fontSize: 50,
+
     }
 
 })
